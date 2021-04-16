30937972fffeb4bfd9efdfada37bec4f01a85e09
raviu-tagline <raviu.tagline@gmail.com>
2021-04-16 18:40:01 +0530

    Commit on 16-04-2021

--- src/calcomponent/TextArea.jsx
+++ src/calcomponent/TextArea.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TextArea = (props) => {
-    return <textarea className="w-50" value={props.ansValue != '' ? props.ansValue : props.queVal} readOnly></textarea>
+    return <textarea style={{width: 85 + '%', resize: 'none', backgroundColor: '#f4f7f8'}} value={props.ansValue != '' ? props.ansValue : props.queVal} readOnly></textarea>
 };
 
 export default TextArea;
